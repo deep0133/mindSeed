9ebca3c6d08e4a1a45b78517f9da419d049809b4
deep0133 <deepanshuheer27@gmail.com>
2025-03-06 13:02:08 +0530

    C:/Program Files/Git/ api added

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+app.use("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Mindseed backend working",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`------App listening on : http://localhost:${PORT}`);
 });
